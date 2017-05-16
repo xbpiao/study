165b19707609f121d63ddbda330ea725bddef44b
xbpiao <xbpiao@126.com>
2017-05-16 19:31:26 +0800

    [update]更新proj.android的工程配置

--- cocos2dx-study/demos/empty_test01/proj.android/jni/Android.mk
+++ cocos2dx-study/demos/empty_test01/proj.android/jni/Android.mk
@@ -11,9 +11,9 @@ LOCAL_SRC_FILES := main.cpp \
                    ../../Classes/HelloWorldScene.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
-                    $(LOCAL_PATH)/../../../../component/cocos2d-x-3.14.1/extensions \
-                    $(LOCAL_PATH)/../../../../component/cocos2d-x-3.14.1 \
-                    $(LOCAL_PATH)/../../../../component/cocos2d-x-3.14.1/cocos/editor-support
+                    $(LOCAL_PATH)/../../../../component/cocos2d-x-3.15/extensions \
+                    $(LOCAL_PATH)/../../../../component/cocos2d-x-3.15 \
+                    $(LOCAL_PATH)/../../../../component/cocos2d-x-3.15/cocos/editor-support
 
 LOCAL_STATIC_LIBRARIES := cocos2dx_static
 
